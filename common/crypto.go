8d7a03208563a55bdc8eec1dcc7acccba9e5f160
Michal Švec <misosvec01@gmail.com>
2025-10-26 15:03:38 +0100

    very basic encryption functions

---
+++ common/crypto.go
@@ -0,0 +1,37 @@
+package common
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+func GenerateKey() []byte {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err)
+	}
+	return key
+}
+
+func Encrypt(plaintext []byte, key []byte) []byte {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		panic(err)
+	}
+
+	return gcm.Seal(nil, nonce, plaintext, nil)
+}
