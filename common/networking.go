7b6129f78bc3017fa23ab8c75725d7fe4092db74
Michal Švec <misosvec01@gmail.com>
2025-11-03 11:59:19 +0100

    additional functions for working with TUN interfaces

--- common/networking.go
+++ common/networking.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"errors"
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -9,7 +10,7 @@ import (
 func GetDefaultRoute() ([]string, error) {
 	out, err := exec.Command("ip", "route", "show", "default").Output()
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("Failed to retrieve routing table: %w", err)
 	}
 
 	lines := strings.Split(strings.TrimSpace(string(out)), "\n")
@@ -21,17 +22,37 @@ func GetDefaultRoute() ([]string, error) {
 	return nil, errors.New("Failed to determine default gateway")
 }
 
-func setDefaultRoute(iface string) error {
-	cmd := exec.Command("ip", "route", "add", "default", "dev", iface)
-	return cmd.Run()
+func SetDefaultRoute(route []string) error {
+	err := exec.Command("ip", "route", "del", "default").Run()
+	if err != nil {
+		return fmt.Errorf("Failed to delete default interface: %w", err)
+	}
+	args := append([]string{"route", "add"}, route...)
+	err = exec.Command("ip", args...).Run()
+	if err != nil {
+		return fmt.Errorf("Failed to set default route %v: %w", route, err)
+	}
+	return nil
 }
 
-func RouteAllPacketsToTun(tunIface string) {
-	if !strings.Contains(strings.ToLower(tunIface), "tun") {
-		panic(errors.New("Provided Interace is not TUN"))
-	}
-	err := setDefaultRoute(tunIface)
+func CreateTunInterface(tunName string) error {
+	err := exec.Command("ip", "tuntap", "add", "dev", tunName, "mode", "tun").Run()
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Failed to create %q interface: %w", tunName, err)
 	}
+
+	err = exec.Command("ip", "link", "set", "dev", tunName, "up").Run()
+	if err != nil {
+		return fmt.Errorf("Failed to enable %q interface: %w", tunName, err)
+	}
+
+	return nil
+}
+
+func DeleteInterface(iface string) error {
+	err := exec.Command("ip", "link", "delete", iface).Run()
+	if err != nil {
+		return fmt.Errorf("Failed to delete %q interface: %w", iface, err)
+	}
+	return nil
 }
